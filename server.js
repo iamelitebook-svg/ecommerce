1b758e2d2f24209964b81c3bb39a8c0e06cb951c
gitpro <iamelitebook@gamil.com>
2026-07-17 22:49:58 +0530

    server api

--- server.js
+++ server.js
@@ -5,25 +5,45 @@ import connectDb from './mongodb.js'
 import productRoutes from './router/productRoutes.js'
 import userRoutes from './router/userRoutes.js'
 import orderRoutes from './router/orderRoutes.js'
+import path from 'path'
 
 
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 dotenv.config()
 connectDb()
 
-const app=express()
+const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
-app.get('/',(req,res)=>{
-    console.log('app running')
-    res.send('api running')
-})
-app.listen(PORT,()=>{
+// app.get('/',(req,res)=>{
+//     console.log('app running')
+//     res.send('api running')
+// })
+
+
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production' || true) {
+    // Serve production assets from the frontend build folder
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    // Point all routing traffic right to your index.html entry point
+    app.get('*', (req, res) =>
+        res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+}
+
+app.listen(PORT, () => {
+    console.log('server online');
+});
+
+app.listen(PORT, () => {
     console.log('server online')
 })

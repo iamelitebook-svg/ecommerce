9d4c5a00f8bf39aca3eef87e57cabaee05bab60b
gitpro <iamelitebook@gamil.com>
2026-07-18 10:29:19 +0530

    server version

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url'
 const PORT = process.env.PORT || 3000
 
 const __filename=fileURLToPath(import.meta.url)
-const __dirname=path.dirname(filename)
+const __dirname=path.dirname(__filename)
 dotenv.config()
 connectDb()
 
